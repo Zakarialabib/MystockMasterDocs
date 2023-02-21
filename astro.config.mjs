c4bc5403e846ddfa5bad859357da12723a8afaf4
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-02-21 01:36:33 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://zakarialabib.github.io',
+  base: '/mystockmasterdocs',
   integrations: [tailwind(), mdx()]
 });
